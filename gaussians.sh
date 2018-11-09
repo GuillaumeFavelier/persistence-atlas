69e2a2c6a7d5ffcc6a9b7faac31bc68cfbd51918
GuillaumeFavelier <guillaume.favelier@gmail.com>
2018-11-09 09:57:08 +0100

    Move the scripts

--- gaussians.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -d gaussians ];
-then
-  mkdir gaussians
-fi
-cd gaussians
-../install/bin/persistenceAtlasCmd -i ../data/main_example.vtu -numberOfNeighbors 4
--- startingVortex.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -d startingVortex ];
-then
-  mkdir startingVortex
-fi
-cd startingVortex
-../install/bin/persistenceAtlasCmd -i ../data/startingVortexGoodEnsemble.vti -numberOfNeighbors 4 -critical 2
