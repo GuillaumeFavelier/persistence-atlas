0757936b39325c5a3107d4ac3e6f679d3f8cac33
GuillaumeFavelier <guillaume.favelier@gmail.com>
2018-11-12 15:28:55 +0100

    Update README

--- scripts/seaSurfaceHeight.sh
+++ scripts/seaSurfaceHeight.sh
@@ -5,4 +5,4 @@ then
   mkdir seaSurfaceHeight
 fi
 cd seaSurfaceHeight
-../install/bin/persistenceAtlasCmd -i ../data/seaSurfaceHeightGoodEnsemble.vti -spread 0.01 -threshold 0.1 | tee seaSurfaceHeight.log
+../install/bin/persistenceAtlasCmd -i ../data/seaSurfaceHeightGoodEnsemble.vti -spread 0.01 -threshold 0.1 -critical 2 | tee seaSurfaceHeight.log
